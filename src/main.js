c97b0313eeec692739673ee25417e87d748cc353
MaytrickChannel <mnithimatechak@cnm.edu>
2024-11-19 20:40:03 -0700

    change the video size factor

--- src/main.js
+++ src/main.js
@@ -2,3 +2,12 @@ import './index.css'
 import 'flowbite'
 
 // This is the main JavaScript file for the project.
+import { Collapse } from 'flowbite';
+const collapse = new Collapse(document.getElementById('navbar-sticky'));
+function closeNavbar() {
+    collapse.collapse()
+}function expandNavbar() {
+    collapse.expand()
+}
+document.closeNavbar = closeNavbar;
+document.expandNavbar = expandNavbar;
